ddb5b580133504428883731af67857afdf506ead
Harshavardhan Veeranna Navalli <harshavardhannavalli3@yahoo.com>
2022-03-28 00:38:55 +0530

    mongodb update

--- database_consumer/database_consumer.py
+++ database_consumer/database_consumer.py
@@ -4,7 +4,14 @@ import time
 from pymongo import MongoClient
 import os
 
-time.sleep(15)
+def insert_into_database(val):
+    conn_string = f'mongodb://mongodb:27017/mydatabase'
+    myclient = MongoClient(conn_string)
+    db = myclient.mydatabase
+    db.ride_requests.insert_one({'data':val})
+
+
+time.sleep(20)
 
 connection=pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel=connection.channel()
@@ -16,7 +23,7 @@ queue=channel.queue_declare(queue='')
 channel.queue_bind(exchange='direct_logs',queue=queue.method.queue,routing_key='database_consumer')
 
 def callback(ch,method,properties,body):
-    print(body)
+    insert_into_database(body)
 
 channel.basic_consume(queue=queue.method.queue,on_message_callback=callback,auto_ack=True)
 
--- ride_matching_consumer/ride_matching_consumer.py
+++ ride_matching_consumer/ride_matching_consumer.py
@@ -4,7 +4,7 @@ import time
 import os
 import requests
 
-time.sleep(15)
+time.sleep(20)
 
 consumer_id=None
 def route():
